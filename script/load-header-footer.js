eae61f405cfa5c7c4b4af1a0bdacb5ebe87d3a6c
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-03-16 14:12:18 +0100

    fix header and footer

--- script/load-header-footer.js
+++ script/load-header-footer.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Load the header
-    fetch('/templates/header.html')
+    fetch('/template/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
         });
-
-    // Load the footer
-    fetch('/templates/footer.html')
+    fetch('/template/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
