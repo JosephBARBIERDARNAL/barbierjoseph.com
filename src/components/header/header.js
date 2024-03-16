ed0d26739d97aaa7e6113438fcba4eff9153ae78
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-03-16 17:05:41 +0100

    update

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import './header.css'; 
+import './header.css';
 
 function Header() {
     return (
         <header>
-            <div class="header-container">
-                <div class="text-and-logos"><br/>
-                    <a href='index.html'><h1>Joseph Barbier</h1></a>
-                    <div class="logos">
-                        <a href="https://www.linkedin.com/in/joseph-barbier-4ab531224/" target="_blank">
-                            <img src={`${process.env.PUBLIC_URL}/img/github.png`} alt="LinkedIn" class="logo"/>
-                        </a>
-                        <a href="https://github.com/JosephBARBIERDARNAL" target="_blank">
-                            <img src={`${process.env.PUBLIC_URL}/img/linkedin.png`} alt="GitHub" class="logo"/>
-                        </a>
-                    </div>
-                </div>
-            </div><br/><br/>
+            <div className="header-container">
+                <a href='/'><h1 class='name'>Joseph Barbier</h1></a>
                 <nav>
-                    <a href="index.html"><b>Home</b></a>
+                    <a href="/"><b>Home</b></a>
                     <a href="work.html"><b>Work</b></a>
                 </nav>
-            </header>
+            </div>
+        </header>
     );
 }
 
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -12,26 +12,28 @@ const Home = () => {
                 <h2>About me</h2><hr />
                 <div className="content-container">
                     <div className="text-content">
-                        <p>Hi! I'm Joseph Barbier.</p>
                         <p>
-                            I am passionate about working in a technical environment and have a keen interest in things related to knowledge.
-                            I am particularly interested in topics such as: how statistics allow us to make <b>inferences</b>, the impact of <b>large language models</b>
-                            and more generally data mining. In my remaining free time, I do sport, read and spend time with my friends.
+                            Hi! I'm Joseph Barbier.
+                        </p>
+                        <p>
+                            I am passionate about working in a technical environment and have a keen interest in things related to knowledge. I am particularly interested in topics such as: how statistics allow us to make <b>inferences</b>, the impact of <b>large language models</b> and more generally data mining. In my remaining free time, I do sport, read and spend time with my friends.
                         </p>
                     </div>
                     <img src={`${process.env.PUBLIC_URL}/img/joseph.png`} alt="Joseph Barbier" className="profile-image" />
                 </div>
                 <p>
                     Since March, I've been working as a data science/viz/software intern with <a href="https://www.yan-holtz.com/" target="_blank" rel="noopener noreferrer">Yan Holtz</a>.
-                </p><br />
+                </p>
+                <br />
 
                 <h2 id="services">Services</h2><hr />
                 <p>I offer <strong>data analysis</strong> and <strong>data visualization</strong> services.
                     I can help you to <strong>understand your data</strong> and create <strong>beautiful visualizations</strong>.
                 </p>
-                <p>If that's something you're interested in, feel free to contact me at
-                    <b>joseph.barbierdarnal@gmail.com</b> and I'd be happy to discuss it with you.
-                </p><br />
+                <p>
+                    If that's something you're interested in, feel free to contact me at <b>joseph.barbierdarnal@gmail.com</b> and I'd be happy to discuss it with you.
+                </p>
+                <br />
 
                 <h2 id="contact">Contact</h2><hr />
                 <p>You can contact me at <b>joseph.barbierdarnal@gmail.com</b> or via <a href="https://www.linkedin.com/in/joseph-barbier-4ab531224/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.
